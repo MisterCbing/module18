0e552bc2f3ee89fe8060eeae6e984a78c20d7872
MisterCbing <da@susanin.news>
2024-11-27 22:16:11 +0400

    Urls и Views

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import class_template, func_template, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('class/', class_template.as_view()),
+    path('func/', func_template),
+    path('', index)
 ]
---
+++ UrbanDjango/task2/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class class_template(TemplateView):
+    template_name = 'class_template.html'
+
+def func_template(request):
+    return render(request, 'func_template.html')
+
+def index(request):
+    return render(request, 'index.html')
+
